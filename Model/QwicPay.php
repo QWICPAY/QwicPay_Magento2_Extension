8e99a4b046a6e7c9307632d19a9e06ad45127f2e
JABeira <joshua@beira.co.za>
2025-04-21 18:25:51 +0200

    ModelLocationFix

--- Model/QwicPay.php
+++ Model/QwicPay.php
@@ -1,18 +1,12 @@
 <?php
-namespace QwicPay\Payment\Model;
+namespace Qwicpay\Checkout\Model;
 
 use Magento\Payment\Model\Method\AbstractMethod;
 
 class QwicPay extends AbstractMethod
 {
     protected $_code = 'qwicpay';
-
-    // Disable capture, refund, etc.
     protected $_isOffline = true;
-    protected $_canAuthorize = false;
-    protected $_canCapture = false;
-    protected $_canRefund = false;
     protected $_canUseCheckout = false;
     protected $_canUseInternal = true;
-    protected $_canUseForMultishipping = false;
 }
